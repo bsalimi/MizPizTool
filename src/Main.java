56afc9833880b7e0f67c76bfb91d8ef5690304a0
bsalimi <bk.salimi@gmail.com>
2014-04-01 12:54:17 -0700

    Test me!

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,6 @@ public class Main {
             System.err.println("Failed to create sessionFactory object." + ex);
             throw new ExceptionInInitializerError(ex);
         }
-        System.out.print("babak kiramo bokhor");
+        System.out.print("dahane amat koni!");
     }
 }
